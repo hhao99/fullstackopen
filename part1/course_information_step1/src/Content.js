6289dbca66290cfd9d49b0e29ae734482a652f5d
Eric Hao <hhao99@gmail.com>
2023-08-19 10:42:05 -0500

    step2 complete

--- part1/course_information_step1/src/Content.js
+++ part1/course_information_step1/src/Content.js
@@ -1,19 +1,20 @@
 import course_info from "./course";
 
+function Part({ part, exercises }) {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+}
 function Content() {
   const { part1, exercises1, part2, exercises2, part3, exercises3 } =
     course_info;
   return (
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
   );
 }
