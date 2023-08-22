6c87da19acaa63da1ff2120790feab4c96e75f9f
Eric Hao <hhao99@gmail.com>
2023-08-22 18:42:38 -0500

    phonebook new

---
+++ part1/phonebook/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import reactLogo from "./assets/react.svg";
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
